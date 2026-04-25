7ff29ca1c56b09f7e351e3985ddb4c43366a1c49
jotaerrec <javi.r49915@gmail.com>
2026-04-25 00:53:24 -0300

    Fix worker entry typing for Cloudflare deploy

--- worker-entry.ts
+++ worker-entry.ts
@@ -1,4 +1,6 @@
 // Wrapper que inyecta nuestro Durable Object al worker generado por OpenNext.
 // `.open-next/worker.js` se genera con `opennextjs-cloudflare build`.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - artifact generado en build time, sin tipos disponibles en este paso.
 export { default } from "./.open-next/worker.js";
 export { CompanyStore } from "./worker/company-store";
